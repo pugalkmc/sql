21c75303748f5b5f2903db3301fce3a4c38e8964
pugal dev <pugalkmc@gmail.com>
2023-02-23 23:48:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def collect_message(update, context):
             save_to_spreadsheet(date_mod.datetime.now().strftime("%Y-%m-%d"))
 
     elif chat_type == "group" or chat_type == "supergroup":
-        if chat_id not in [-1001588000922] or username not in ["Jellys04","Cryptomaker143" "Shankar332", "Royce73",
+        if chat_id not in [-1001588000922] or username not in ["Jellys04","Cryptomaker143", "Shankar332", "Royce73",
                                                                                                "Balaharishb",
                                                                                                "LEO_sweet_67",
                                                                                                "SaranKMC", "pugalkmc"]:
@@ -68,27 +68,35 @@ def collect_message(update, context):
         # Only process messages from specific users in personal chat
         collection_name = date_mod.datetime.now().strftime("%Y-%m-%d")
         message_id = message.message_id
-        message_date = message.date
+        message_date = message.date.strftime("%Y-%m-%d %H:%M:%S")  # Convert datetime to string
         message_text = message.text
         insert_query = f"INSERT INTO messages (username, message_id, message_text, message_date) VALUES ('{username}', '{message_id}', '{message_text}', '{message_date}')"
         cursor.execute(insert_query)
         conn.commit()
 
 
-def save_to_spreadsheet(admin="no",update=None, context=None, date=None):
-    collection_name = date_mod.datetime.now().strftime("%Y-%m-%d") if date is None else date
 
-    # Get all the messages from the database
-    select_query = f"SELECT * FROM messages WHERE message_date >= '{collection_name}' AND message_date < '{collection_name} 23:59:59'"
+def save_to_spreadsheet(admin="yes", update=None, context=None, date=None):
+    collection_name = datetime.now().strftime("%Y-%m-%d") if date is None else date
+
+    # Get all the messages from the database for a specific date
+    select_query = f"SELECT username, message_id, message_text, message_date FROM messages WHERE DATE(message_date) = '{collection_name}'"
     cursor.execute(select_query)
-    messages = [{'username': row[0], 'message_id': row[1], 'message_text': row[2], 'message_date': row[3]} for row in
-                cursor.fetchall()]
+    messages = [{'username': row[0], 'message_id': row[1], 'message_text': row[2], 'message_date': row[3]} for row in cursor.fetchall()]
+
+    # Count the number of messages from each user
+    user_counts = {}
+    for message in messages:
+        if message['username'] in user_counts:
+            user_counts[message['username']] += 1
+        else:
+            user_counts[message['username']] = 1
 
     # Create a new Excel workbook and worksheet
     wb = openpyxl.Workbook()
     ws = wb.active
 
-    # Write the headers
+    # Write the headers and user message counts
     ws.column_dimensions['A'].width = 13
     ws.column_dimensions['B'].width = 30
     ws.column_dimensions['C'].width = 25
@@ -97,13 +105,20 @@ def save_to_spreadsheet(admin="no",update=None, context=None, date=None):
     ws['B1'] = 'Message Link'
     ws['C1'] = 'Message Text'
     ws['D1'] = 'Message Date'
+    ws['E1'] = 'Message Count'
+    for i, (username, count) in enumerate(user_counts.items(), start=2):
+        ws.cell(row=i, column=1, value=username)
+        ws.cell(row=i, column=5, value=count)
 
     # Write the data to the worksheet
-    for i, message in enumerate(messages, start=2):
+    for i, message in enumerate(messages, start=len(user_counts) + 2):
         ws.cell(row=i, column=1, value=message['username'])
         ws.cell(row=i, column=2, value=f"https://t.me/poolsea/{message['message_id']}")
         ws.cell(row=i, column=3, value=message['message_text'])
         ws.cell(row=i, column=4, value=message['message_date'])
+
+    # Freeze the top row of the sheet
+    ws.freeze_panes = ws['A2']
 
     # Save the workbook
     wb.save('chat_history.xlsx')
